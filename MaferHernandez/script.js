02f34d679c24cabdc35720d3bae5bf1cf66f6fe6
Mafer_Hernandez <hernandezmf05@hotmail.com>
2025-11-13 12:04:09 -0500

    Feat se realiza el segundo punto del taller

--- MaferHernandez/script.js
+++ MaferHernandez/script.js
@@ -1,10 +1,22 @@
+/* primer punto*/
 //Pedir el precio de 3 productos 
-let productoUno = Number (prompt ("Ingrese el precio del producto 1"))
-let productoDos = Number (prompt ("Ingrese el precio del producto 2"))
-let productoTres= Number (prompt ("Ingrese el precio del producto 3"))
+let productoUno = Number (prompt ("Ingrese el precio del producto 1"));
+let productoDos = Number (prompt ("Ingrese el precio del producto 2"));
+let productoTres= Number (prompt ("Ingrese el precio del producto 3"));
 
 //calcular el total de los productos 
-let totalAPagar = (productoUno + productoDos + productoTres)
+let totalAPagar = (productoUno + productoDos + productoTres); 
 
 //mostrar el total a pagar 
-console.log( "el total a pagar es: " + totalAPagar)
+console.log( "el total a pagar es: " + totalAPagar);
+
+/* segundo punto*/
+//pedir el ancho y el alto de una pared
+let anchoPared = Number (prompt ("Ingrese el ancho de la pared: "));
+let altoPared = Number (prompt ("Ingrese el alto de la pared: "));
+
+//calcular el area de la pared
+let areaPared= (anchoPared * altoPared);
+
+//mostrar el area de la pared 
+console.log("El area de la pared es: " + areaPared);
