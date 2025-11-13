70c3eef706043729e8b6aafa7b87bae1ee1330f3
Mafer_Hernandez <hernandezmf05@hotmail.com>
2025-11-13 11:49:46 -0500

    Feat se realiza el primer punto del taller

---
+++ MaferHernandez/script.js
@@ -0,0 +1,10 @@
+//Pedir el precio de 3 productos 
+let productoUno = Number (prompt ("Ingrese el precio del producto 1"))
+let productoDos = Number (prompt ("Ingrese el precio del producto 2"))
+let productoTres= Number (prompt ("Ingrese el precio del producto 3"))
+
+//calcular el total de los productos 
+let totalAPagar = (productoUno + productoDos + productoTres)
+
+//mostrar el total a pagar 
+console.log( "el total a pagar es: " + totalAPagar)
