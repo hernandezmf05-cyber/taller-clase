22f334db17cd7627dfb12d27701f627b6ab45665
Mafer_Hernandez <hernandezmf05@hotmail.com>
2025-11-16 12:22:12 -0500

    feat se realiza el decimo punto del taller

--- MaferHernandez/script.js
+++ MaferHernandez/script.js
@@ -1,87 +1,87 @@
  /* primer punto*/
 //Pedir el precio de 3 productos 
-// let productoUno = Number (prompt ("Ingrese el precio del producto 1"));
-// let productoDos = Number (prompt ("Ingrese el precio del producto 2"));
-// let productoTres= Number (prompt ("Ingrese el precio del producto 3"));
+let productoUno = Number (prompt ("Ingrese el precio del producto 1"));
+let productoDos = Number (prompt ("Ingrese el precio del producto 2"));
+let productoTres= Number (prompt ("Ingrese el precio del producto 3"));
 
-// //calcular el total de los productos 
-// let totalAPagar = (productoUno + productoDos + productoTres); 
+//calcular el total de los productos 
+let totalAPagar = (productoUno + productoDos + productoTres); 
 
-// //mostrar el total a pagar 
-// console.log( "el total a pagar es: " + totalAPagar);
+//mostrar el total a pagar 
+console.log( "el total a pagar es: " + totalAPagar);
 
-// /* segundo punto*/
-// //pedir el ancho y el alto de una pared
-// let anchoPared = Number (prompt ("Ingrese el ancho de la pared: "));
-// let altoPared = Number (prompt ("Ingrese el alto de la pared: "));
+/* segundo punto*/
+//pedir el ancho y el alto de una pared
+let anchoPared = Number (prompt ("Ingrese el ancho de la pared: "));
+let altoPared = Number (prompt ("Ingrese el alto de la pared: "));
 
-// //calcular el area de la pared
-// let areaPared= (anchoPared * altoPared);
+//calcular el area de la pared
+let areaPared= (anchoPared * altoPared);
 
-// //mostrar el area de la pared 
-// console.log("El area de la pared es: " + areaPared);
+//mostrar el area de la pared 
+console.log("El area de la pared es: " + areaPared);
 
-// /*tercer punto*/
-// //pide el largo del ancho y el largo del protero
-// let largoPotrero = Number (prompt ("Ingrese el largo del potrero: "));
-// let anchoPotrero = Number (prompt ("Ingrese el ancho del potrero: "));
+/*tercer punto*/
+//pide el largo del ancho y el largo del protero
+let largoPotrero = Number (prompt ("Ingrese el largo del potrero: "));
+let anchoPotrero = Number (prompt ("Ingrese el ancho del potrero: "));
 
-// //calcular el perimetro
-// let perimetroPotrero = 2 * (largoPotrero + anchoPotrero);
+//calcular el perimetro
+let perimetroPotrero = 2 * (largoPotrero + anchoPotrero);
 
-// //mostrar el perimetro del potrero
-// console.log("El perimetro del potrero es: " + perimetroPotrero);
+//mostrar el perimetro del potrero
+console.log("El perimetro del potrero es: " + perimetroPotrero);
 
-// /*cuarto punto*/
-// //Pide la temperatura en grados °F
-// let temperaturaFahrenheit = Number (prompt("Ingrese la temperatura en grados °F: "));
+/*cuarto punto*/
+//Pide la temperatura en grados °F
+let temperaturaFahrenheit = Number (prompt("Ingrese la temperatura en grados °F: "));
 
-// // convertir en grados °C 
-// let temperaturaeraturaCelsius = (temperaturaFahrenheit - 32) / 1.8;
+// convertir en grados °C 
+let temperaturaeraturaCelsius = (temperaturaFahrenheit - 32) / 1.8;
 
-// //mostrar el resultado de la conversion
-// console.log("La temperatura en grados celsius es: " + temperaturaeraturaCelsius);
+//mostrar el resultado de la conversion
+console.log("La temperatura en grados celsius es: " + temperaturaeraturaCelsius);
 
-// /*quinto punto*/
-// // Pide el año de nacimiento 
-// let añoNacimiento = Number (prompt ("Ingrese su año de nacimiento: "));
+/*quinto punto*/
+// Pide el año de nacimiento 
+let añoNacimiento = Number (prompt ("Ingrese su año de nacimiento: "));
 
-// //calcular cuantos años tiene para el 2024 
-// let edad = añoNacimiento - 2024;
+//calcular cuantos años tiene para el 2024 
+let edad = añoNacimiento - 2024;
 
-// //mostrar la edad que tiene
-// console.log("Tienes" + edad + "años");
+//mostrar la edad que tiene
+console.log("Tienes" + edad + "años");
 
-// /* sexto punto*/
-// //Pedir nombre  y el apellido del usuario
-// let nombre = (prompt ("Ingrese su nombre: "));
-// let apellido = (prompt ("Ingrese su apellido: "));
+/* sexto punto*/
+//Pedir nombre  y el apellido del usuario
+let nombre = (prompt ("Ingrese su nombre: "));
+let apellido = (prompt ("Ingrese su apellido: "));
 
-// // mostrar mensaje
-// console.log("Bienvenido, " + nombre + apellido);
+// mostrar mensaje
+console.log("Bienvenido, " + nombre + apellido);
 
-// /* septimo punto*/
-// //pedir 3 notas de 0 a 5
-// let nota1 = Number (prompt("Ingrese de 0 a 5, la primera nota: "));
-// let nota2 = Number (prompt("Ingrese de 0 a 5, la segunda nota: "));
-// let nota3 = Number (prompt("Ingrese de 0 a 5, la tercera< nota: "));
+/* septimo punto*/
+//pedir 3 notas de 0 a 5
+let nota1 = Number (prompt("Ingrese de 0 a 5, la primera nota: "));
+let nota2 = Number (prompt("Ingrese de 0 a 5, la segunda nota: "));
+let nota3 = Number (prompt("Ingrese de 0 a 5, la tercera< nota: "));
 
-// //calcular el promedio 
-// let promedio= (nota1 + nota2 + nota3) / 3;
+//calcular el promedio 
+let promedio= (nota1 + nota2 + nota3) / 3;
 
-// //mostrar promedio
-// console.log("El promedio de notas es:  " + promedio);
+//mostrar promedio
+console.log("El promedio de notas es:  " + promedio);
 
-// /*octavo punto*/
-// //pedir el numero de horas trabajadas en la semana y el precio que se paga por hora
-// let numeroHorasTrabajadas = Number (prompt("Ingrese el numero de horas trabajadas en la semana: "));
-// let precioHora = Number (prompt("Ingrese el precio de la hora trabajada: "));
+/*octavo punto*/
+//pedir el numero de horas trabajadas en la semana y el precio que se paga por hora
+let numeroHorasTrabajadas = Number (prompt("Ingrese el numero de horas trabajadas en la semana: "));
+let precioHora = Number (prompt("Ingrese el precio de la hora trabajada: "));
 
-// //calcular el salario total
-// let salarioTotal = numeroHorasTrabajadas * precioHora; 
+//calcular el salario total
+let salarioTotal = numeroHorasTrabajadas * precioHora; 
 
-// //mostrar salario 
-// console.log("El salario semanal es: " + salarioTotal);
+//mostrar salario 
+console.log("El salario semanal es: " + salarioTotal);
 
 /*noveno punto*/
 let distanciaMillas = Number (prompt("Ingrese la distancia en millas: "));
@@ -89,3 +89,11 @@ let distanciaMillas = Number (prompt("Ingrese la distancia en millas: "));
 let kilometros= distanciaMillas * 1.60934;
 
 console.log("La distancia en kilometros es: " + kilometros);
+
+/*decimo punto*/
+let totalMinutos = Number (prompt("Ingrese el total de minutos trabajados : "));
+
+let horas= totalMinutos / 60;
+let minutos = totalMinutos % 60;
+
+console.log( totalMinutos + " minutos equivalen a " + horas + " horas y " + minutos + " minutos")
